ee438d84c6d5dda2960ebd5ac9f81d0d6f26439b
haschtl <haschtl@pm.me>
2026-02-17 13:36:51 +0100

    add android icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,48 @@ const config: CapacitorConfig = {
   appId: "app.domora.mobile",
   appName: "Domora",
   webDir: "dist",
-  bundledWebRuntime: false,
+  loggingBehavior: "debug",
+  backgroundColor: "#f0f5f4",
+  zoomEnabled: false,
+  initialFocus: true,
+  android: {
+    backgroundColor: "#f0f5f4",
+    zoomEnabled: false,
+    allowMixedContent: false,
+    captureInput: true,
+    minWebViewVersion: 80,
+    resolveServiceWorkerRequests: true
+  },
+  ios: {
+    backgroundColor: "#f0f5f4",
+    zoomEnabled: false,
+    contentInset: "never",
+    scrollEnabled: true,
+    allowsLinkPreview: false,
+    preferredContentMode: "recommended",
+    handleApplicationNotifications: true
+  },
   server: {
+    hostname: "localhost",
+    iosScheme: "capacitor",
     androidScheme: "https"
+  },
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: true,
+      launchShowDuration: 300,
+      showSpinner: false,
+      backgroundColor: "#f0f5f4",
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: true,
+      splashImmersive: false
+    },
+    CapacitorHttp: {
+      enabled: true
+    },
+    SystemBars: {
+      insetsHandling: "css"
+    }
   }
 };
 
